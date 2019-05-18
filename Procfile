e6a89b8904dfa8fe21bc766f8b582b30921b25fb
sbogdan60 <sbogdan.sb@gmail.com>
2019-05-17 21:44:08 -0700

    adding Heroku depenencies

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: index.html
+web: gunicorn app:app
---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
